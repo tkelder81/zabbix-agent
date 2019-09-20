991afb6356a462b3a1db3dab1f3ab9b485dc920e
tkelder81 <55492017+tkelder81@users.noreply.github.com>
2019-09-20 16:21:18 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,3 @@
 New-Item -ErrorAction Ignore -Path 'c:\temp' -ItemType Directory
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/tkelder81/zabbix-agent/master/install.cmd" -OutFile "C:\temp\install.cmd"
-Start-Process "C:\temp\install.cmd"
+"C:\temp\install.cmd"
