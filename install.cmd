1eb2e3a24594413ea2bd969e7114a2f353a53503
tkelder81 <55492017+tkelder81@users.noreply.github.com>
2019-09-20 16:28:29 +0200

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -4,7 +4,7 @@ echo.
 if not exist ping.exe goto NotAnAdministrator
 
 
-# "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+rem "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 echo.
 
 set /P hostname=Please enter the Hostname as configured in Zabbix:
