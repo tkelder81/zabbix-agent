f382c7c9cc40d9e95bd00d159ab4bf8c900026e0
tkelder81 <55492017+tkelder81@users.noreply.github.com>
2019-09-20 16:14:18 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force
+New-Item -ErrorAction Ignore -Path 'c:\temp' -ItemType Directory
 New-Item -Path 'c:\temp' -ItemType Directory
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/tkelder81/zabbix-agent/master/install.cmd" -OutFile "C:\temp\install.cmd"
 Start-Process "cmd.exe"  "/c C:\temp\install.cmd"
